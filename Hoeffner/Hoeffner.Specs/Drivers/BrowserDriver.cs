992c7106c2f84dffdc71ea69baaa6659a028bf8c
ZekoTheFirst <61459280+ZekoTheFirst@users.noreply.github.com>
2023-11-29 17:39:29 +0100

    Update BrowserDriver.cs

--- Hoeffner/Hoeffner.Specs/Drivers/BrowserDriver.cs
+++ Hoeffner/Hoeffner.Specs/Drivers/BrowserDriver.cs
@@ -1,10 +1,5 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 
 namespace Hoeffner.Specs.Drivers
